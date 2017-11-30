bb6817e24b209e829d37141f15d907fc6010447f
Clyde Brown <clyde@mapper.ai>
2017-11-30 12:10:16 -0800

    remove dead script

--- compass.rb
+++
@@ -1,8 +0,0 @@
-project_path = File.expand_path(File.join(File.dirname(__FILE__), './src/assets'))
-add_import_path "#{project_path}"
-add_import_path "#{project_path}/styles"
-
-sass_dir = %w(
-	"#{project_path}"
-	"#{project_path}/styles"
-)
