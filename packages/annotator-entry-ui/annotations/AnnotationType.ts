a97f984e041d85892453cdeea6a379512a8b56ed
Clyde Brown <clyde@mapper.ai>
2018-01-04 17:29:48 -0800

    redo v3 serialization format with all annotations in one array and explicit annotationType; support forward conversion from v2 documents

---
+++ packages/annotator-entry-ui/annotations/AnnotationType.ts
@@ -0,0 +1,11 @@
+/**
+ *  Copyright 2017 Mapper Inc.
+ *  CONFIDENTIAL. AUTHORIZED USE ONLY. DO NOT REDISTRIBUTE.
+ */
+
+export enum AnnotationType {
+	UNKNOWN,
+	LANE,
+	TRAFFIC_SIGN,
+	CONNECTION
+}
