7da32dd2544a3902e2d5ee55b4cdb2139e8688a8
Andrei <cosmac@gwmail.gwu.edu>
2018-01-05 22:03:08 -0800

    nitpicking

--- packages/annotator-entry-main/MainEntry.ts
+++ packages/annotator-entry-main/MainEntry.ts
@@ -9,8 +9,8 @@ const
 	url = require('url'),
 	Path = require('path')
 
+// Ask for ~6GB memory
 app.commandLine.appendSwitch('js-flags', '--max-old-space-size=6096')
-
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
