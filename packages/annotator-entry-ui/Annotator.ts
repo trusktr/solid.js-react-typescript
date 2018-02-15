a9730eeae35983139cdc1081ba6fad1cf90f066e
Chris Thompson <chris@mapper.ai>
2018-02-15 12:35:08 -0500

    Fixed CoordinateFrameType after failed experiment.  Listening for data on /tmp/mapper/InertialState

--- packages/annotator-entry-ui/Annotator.ts
+++ packages/annotator-entry-ui/Annotator.ts
@@ -414,10 +414,10 @@ class Annotator {
 			&& state.pose.q0 !== null && state.pose.q1 !== null && state.pose.q2 !== null && state.pose.q3 !== null
 		) {
 			const inputPosition = new THREE.Vector3(state.pose.x, state.pose.y, state.pose.z)
-			const standardPosition = convertToStandardCoordinateFrame(inputPosition, CoordinateFrameType.LIDAR)
+			const standardPosition = convertToStandardCoordinateFrame(inputPosition, CoordinateFrameType.STANDARD)
 			const positionThreeJs = this.tileManager.utmToThreeJs(standardPosition.x, standardPosition.y, standardPosition.z)
 			const inputRotation = new THREE.Quaternion(state.pose.q0, state.pose.q1, state.pose.q2, state.pose.q3)
-			const standardRotation = cvtQuaternionToStandardCoordinateFrame(inputRotation, CoordinateFrameType.LIDAR)
+			const standardRotation = cvtQuaternionToStandardCoordinateFrame(inputRotation, CoordinateFrameType.STANDARD)
 			const rotationThreeJs = new THREE.Quaternion(standardRotation.y, standardRotation.z, standardRotation.x, standardRotation.w)
 			rotationThreeJs.normalize()
 
@@ -518,7 +518,7 @@ class Annotator {
 	private loadPointCloudData(pathToTiles: string): Promise<void> {
 		if (!this.uiState.isPointCloudVisible)
 			this.setModelVisibility(ModelVisibility.ALL_VISIBLE)
-		return this.tileManager.loadFromDataset(pathToTiles, CoordinateFrameType.LIDAR)
+		return this.tileManager.loadFromDataset(pathToTiles, CoordinateFrameType.STANDARD)
 			.then(() => {
 				if (!this.annotationManager.setOriginWithInterface(this.tileManager))
 					log.warn(`annotations origin ${this.annotationManager.getOrigin()} does not match tile's origin ${this.tileManager.getOrigin()}`)
@@ -1937,7 +1937,7 @@ class Annotator {
 				log.warn('got an InertialStateMessage without a pose')
 		})
 
-		this.liveSubscribeSocket.connect("ipc:///tmp/InertialState")
+		this.liveSubscribeSocket.connect("ipc:///tmp/mapper/InertialState")
 		this.liveSubscribeSocket.subscribe("")
 	}
 
