b71867217ed6b5c8731001b3a7ded7edee719cb4
Joe Pea <joe@trusktr.io>
2018-06-25 17:54:32 -0700

    fix indentation in StatusWindow.tsx

--- src/annotator-z-hydra-shared/components/StatusWindow.tsx
+++ src/annotator-z-hydra-shared/components/StatusWindow.tsx
@@ -17,136 +17,129 @@ import {StatusKey} from "@/annotator-z-hydra-shared/src/models/StatusKey";
 import StatusWindowActions from "@/annotator-z-hydra-shared/StatusWindowActions";
 import {UtmCoordinateSystem} from "@/annotator-entry-ui/UtmCoordinateSystem";
 import {
-  LocationServerStatusClient,
-  LocationServerStatusLevel
+    LocationServerStatusClient,
+    LocationServerStatusLevel
 } from "@/annotator-entry-ui/status/LocationServerStatusClient";
 import Logger from "@/util/log";
 
 const log = Logger(__filename)
 
 interface StatusWindowProps {
-	statusWindowState ?: StatusWindowState
-	liveModeEnabled ?: boolean
-	playModeEnabled ?: boolean
-  utmCoordinateSystem: UtmCoordinateSystem
+    statusWindowState ?: StatusWindowState
+    liveModeEnabled ?: boolean
+    playModeEnabled ?: boolean
+    utmCoordinateSystem: UtmCoordinateSystem
 }
 
 interface IStatusWindowState {
-  locationServerStatusDisplayTimer: number
-  timeToDisplayHealthyStatusMs: number
-  locationServerStatusClient: LocationServerStatusClient
+    locationServerStatusDisplayTimer: number
+    timeToDisplayHealthyStatusMs: number
+    locationServerStatusClient: LocationServerStatusClient
 }
 
 
 @typedConnect(createStructuredSelector({
-	statusWindowState: (state) => state.get(RoadEditorState.Key).statusWindowState,
+    statusWindowState: (state) => state.get(RoadEditorState.Key).statusWindowState,
 }))
 export default class StatusWindow extends React.Component<StatusWindowProps, IStatusWindowState> {
 
-	constructor(props: StatusWindowProps) {
-		super(props)
+    constructor(props: StatusWindowProps) {
+        super(props)
 
-    const locationServerStatusClient = new LocationServerStatusClient(this.onLocationServerStatusUpdate)
+        const locationServerStatusClient = new LocationServerStatusClient(this.onLocationServerStatusUpdate)
 
-		this.state = {
-      locationServerStatusDisplayTimer: 0,
-      timeToDisplayHealthyStatusMs: 10000,
-      locationServerStatusClient: locationServerStatusClient
-		}
+        this.state = {
+            locationServerStatusDisplayTimer: 0,
+            timeToDisplayHealthyStatusMs: 10000,
+            locationServerStatusClient: locationServerStatusClient
+        }
 
-		locationServerStatusClient.connect()
+        locationServerStatusClient.connect()
 
-	}
-
-	updateCurrentLocationStatusMessage(positionUtm: THREE.Vector3): void {
-    // This is a hack to allow data with no coordinate reference system to pass through the UTM classes.
-    // Data in local coordinate systems tend to have small values for X (and Y and Z) which are invalid in UTM.
-    if (positionUtm.x > 100000) { // If it looks local, don't convert to LLA. TODO fix this.
-      const positionLla = this.props.utmCoordinateSystem.utmVectorToLngLatAlt(positionUtm)
-      const messageLla = sprintf('LLA: %.4fE %.4fN %.1falt', positionLla.x, positionLla.y, positionLla.z)
-
-      // this.statusWindow.setMessage(statusKey.currentLocationLla, messageLla)
-      new StatusWindowActions().setMessage(StatusKey.CURRENT_LOCATION_LLA, messageLla)
     }
-    const messageUtm = sprintf('UTM %s: %dE %dN %.1falt', this.props.utmCoordinateSystem.utmZoneString(), positionUtm.x, positionUtm.y, positionUtm.z)
-    // this.statusWindow.setMessage(statusKey.currentLocationUtm, messageUtm)
-    new StatusWindowActions().setMessage(StatusKey.CURRENT_LOCATION_UTM, messageUtm)
-  }
 
+    updateCurrentLocationStatusMessage(positionUtm: THREE.Vector3): void {
+        // This is a hack to allow data with no coordinate reference system to pass through the UTM classes.
+        // Data in local coordinate systems tend to have small values for X (and Y and Z) which are invalid in UTM.
+        if (positionUtm.x > 100000) { // If it looks local, don't convert to LLA. TODO fix this.
+            const positionLla = this.props.utmCoordinateSystem.utmVectorToLngLatAlt(positionUtm)
+            const messageLla = sprintf('LLA: %.4fE %.4fN %.1falt', positionLla.x, positionLla.y, positionLla.z)
 
-	render(): JSX.Element {
-		const {statusWindowState} = this.props
-		// const isEnabled = getValue(() => statusWindowState.enabled, false)
-		const messages = getValue(() => statusWindowState && statusWindowState.messages, new Map<string, string>()) as Map<string, string>
-
-		let out = ''
-		messages.forEach(value => {
-			if (value !== '')
-				out += value + '<br>'
-		})
-		// this.statusElement.innerHTML = out
-		// const laneWidth = $('#lp_width_value')
-
-		// @TODO show/hide internal parts of the component based on the value of isEnabled
-		return (
-			<div id="status_window">
-				STATUS WINDOW <br/>
-				<span dangerouslySetInnerHTML={{__html: out}} />
-			</div>
-		)
-	}
-
-
-
-
-  // Display a UI element to tell the user what is happening with the location server.
-  // Error messages persist, and success messages disappear after a time-out.
-  onLocationServerStatusUpdate: (level: LocationServerStatusLevel, serverStatus: string)
-    => void = (level: LocationServerStatusLevel, serverStatus: string) => {
-
-    let message = 'Location status: '
-    switch (level) {
-      case LocationServerStatusLevel.INFO:
-        message += '<span class="statusOk">' + serverStatus + '</span>'
-        this.delayLocationServerStatus()
-        break
-      case LocationServerStatusLevel.WARNING:
-        message += '<span class="statusWarning">' + serverStatus + '</span>'
-        this.cancelHideLocationServerStatus()
-        break
-      case LocationServerStatusLevel.ERROR:
-        message += '<span class="statusError">' + serverStatus + '</span>'
-        this.cancelHideLocationServerStatus()
-        break
-      default:
-        log.error('unknown LocationServerStatusLevel ' + LocationServerStatusLevel.ERROR)
+            // this.statusWindow.setMessage(statusKey.currentLocationLla, messageLla)
+            new StatusWindowActions().setMessage(StatusKey.CURRENT_LOCATION_LLA, messageLla)
+        }
+        const messageUtm = sprintf('UTM %s: %dE %dN %.1falt', this.props.utmCoordinateSystem.utmZoneString(), positionUtm.x, positionUtm.y, positionUtm.z)
+        // this.statusWindow.setMessage(statusKey.currentLocationUtm, messageUtm)
+        new StatusWindowActions().setMessage(StatusKey.CURRENT_LOCATION_UTM, messageUtm)
     }
-    new StatusWindowActions().setMessage(StatusKey.LOCATION_SERVER, message)
-  }
-
-  private delayLocationServerStatus = (): void => {
-    this.cancelHideLocationServerStatus()
-    this.hideLocationServerStatus()
-  }
-
-  private cancelHideLocationServerStatus = (): void => {
-    if (this.state.locationServerStatusDisplayTimer)
-      window.clearTimeout(this.state.locationServerStatusDisplayTimer)
-  }
-
-  private hideLocationServerStatus = (): void => {
-    const locationServerStatusDisplayTimer = window.setTimeout(() => {
-      new StatusWindowActions().setMessage(StatusKey.LOCATION_SERVER, '')
-    }, this.state.timeToDisplayHealthyStatusMs)
-
-		this.setState({locationServerStatusDisplayTimer})
-  }
 
 
+    render(): JSX.Element {
+        const {statusWindowState} = this.props
+        // const isEnabled = getValue(() => statusWindowState.enabled, false)
+        const messages = getValue(() => statusWindowState && statusWindowState.messages, new Map<string, string>()) as Map<string, string>
+
+        let out = ''
+        messages.forEach(value => {
+            if (value !== '')
+            out += value + '<br>'
+        })
+        // this.statusElement.innerHTML = out
+        // const laneWidth = $('#lp_width_value')
+
+        // @TODO show/hide internal parts of the component based on the value of isEnabled
+        return (
+            <div id="status_window">
+                STATUS WINDOW <br/>
+                <span dangerouslySetInnerHTML={{__html: out}} />
+            </div>
+        )
+    }
 
 
 
 
+    // Display a UI element to tell the user what is happening with the location server.
+    // Error messages persist, and success messages disappear after a time-out.
+    onLocationServerStatusUpdate: (level: LocationServerStatusLevel, serverStatus: string) => void =
+        (level: LocationServerStatusLevel, serverStatus: string) => {
 
+        let message = 'Location status: '
+        switch (level) {
+            case LocationServerStatusLevel.INFO:
+            message += '<span class="statusOk">' + serverStatus + '</span>'
+            this.delayLocationServerStatus()
+            break
+            case LocationServerStatusLevel.WARNING:
+            message += '<span class="statusWarning">' + serverStatus + '</span>'
+            this.cancelHideLocationServerStatus()
+            break
+            case LocationServerStatusLevel.ERROR:
+            message += '<span class="statusError">' + serverStatus + '</span>'
+            this.cancelHideLocationServerStatus()
+            break
+            default:
+            log.error('unknown LocationServerStatusLevel ' + LocationServerStatusLevel.ERROR)
+        }
+        new StatusWindowActions().setMessage(StatusKey.LOCATION_SERVER, message)
+    }
+
+    private delayLocationServerStatus = (): void => {
+        this.cancelHideLocationServerStatus()
+        this.hideLocationServerStatus()
+    }
+
+    private cancelHideLocationServerStatus = (): void => {
+        if (this.state.locationServerStatusDisplayTimer)
+        window.clearTimeout(this.state.locationServerStatusDisplayTimer)
+    }
+
+    private hideLocationServerStatus = (): void => {
+        const locationServerStatusDisplayTimer = window.setTimeout(() => {
+            new StatusWindowActions().setMessage(StatusKey.LOCATION_SERVER, '')
+        }, this.state.timeToDisplayHealthyStatusMs)
+
+        this.setState({locationServerStatusDisplayTimer})
+    }
 
 }
