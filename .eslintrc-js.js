b9f2b1f4fcdb756d10a1097b20fbdbe25e35eeda
Joe Pea <joe@trusktr.io>
2018-08-01 11:26:00 -0700

    make separate eslintrc for JS and TS that inherit from the base config. It's easier to understand, and works better (it was running TS rules on JS)

--- .eslintrc-js.js
+++ .eslintrc-js.js
@@ -1,114 +1,11 @@
-// http://eslint.org/docs/user-guide/configuring
+// ESLint rules for JS only
+
+console.log('ESLint JavaScript config')
 
 module.exports = {
-	root: true,
-	// parser: 'babel-eslint',
-	parser: 'typescript-eslint-parser',
-	parserOptions: {
-		sourceType: 'module',
-	},
-	env: {
-		browser: true,
-		node: true,
-		es6: true,
-	},
-	// https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-	extends: [
-		'plugin:vue/base',
-		'standard',
-	],
-	// required to lint *.vue files
+	extends: ['./.eslintrc.js'],
+	parser: 'babel-eslint',
 	plugins: [
-		'html',
-		'vue',
 		'json',
-		'typescript',
 	],
-	// add your custom rules here
-	rules: {
-
-		// disable this rule because it causes an error with our code.
-		'no-useless-constructor': 0,
-
-		// allow paren-less arrow functions
-		'arrow-parens': 0,
-		// allow async-await
-		'generator-star-spacing': 0,
-		// allow debugger during development
-		'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-
-		'no-tabs': 0,
-		indent: ['error', 'tab', {SwitchCase: 1}],
-		'prefer-const': 'error',
-		'one-var': ['error', 'never'],
-		'no-var': 'error',
-		'space-in-parens': ['error', 'never'],
-		'space-before-function-paren': ['error', 'never'],
-		'padded-blocks': ['error', 'never'],
-		'comma-dangle': ['error', 'always-multiline'],
-		'template-curly-spacing': ['error', 'never'],
-		'promise/param-names': 0,
-		'no-return-assign': ['error', 'except-parens'],
-		'object-curly-spacing': ['error', 'never'],
-		'array-bracket-spacing': ['error', 'never'],
-		'computed-property-spacing': ['error', 'never'],
-		'brace-style': ['error', '1tbs', {allowSingleLine: false}],
-		'quote-props': ['error', 'as-needed'],
-		curly: ['error', 'multi-or-nest', 'consistent'],
-
-		'padding-line-between-statements': ['error',
-
-			{
-				blankLine: 'always',
-				prev: '*',
-				next: [
-					'block',
-					'const',
-					'let',
-					'var',
-					'import',
-					'export',
-				],
-			},
-			{
-				blankLine: 'always',
-				next: '*',
-				prev: [
-					'block',
-					'const',
-					'let',
-					'var',
-					'import',
-					'export',
-				],
-			},
-
-			{blankLine: 'never', prev: 'const', next: 'const'},
-			{blankLine: 'never', prev: 'let', next: 'let'},
-			{blankLine: 'never', prev: 'var', next: 'var'},
-			{blankLine: 'never', prev: 'import', next: 'import'},
-			{blankLine: 'never', prev: 'export', next: 'export'},
-
-			{
-				blankLine: 'always',
-				prev: '*',
-				next: [
-					'multiline-block-like',
-					'multiline-expression',
-					'class',
-					'function',
-				],
-			},
-			{
-				blankLine: 'always',
-				next: '*',
-				prev: [
-					'multiline-block-like',
-					'multiline-expression',
-					'class',
-					'function',
-				],
-			},
-		],
-	},
 }
