5782efb766eadf70979281f0ac176a84c8b2ca24
Joe Pea <joe@trusktr.io>
2018-08-22 14:22:10 -0700

    remove annotated-scene from the repo

--- annotated-scene/src/models/CameraType.ts
+++
@@ -1,9 +0,0 @@
-/**
- *  Copyright 2018 Mapper Inc.
- *  CONFIDENTIAL. AUTHORIZED USE ONLY. DO NOT REDISTRIBUTE.
- */
-
-export enum CameraType {
-	ORTHOGRAPHIC = 'orthographic',
-	PERSPECTIVE = 'perspective',
-}
--- annotated-scene/src/types/globals.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- *  Copyright 2018 Mapper Inc.
- *  CONFIDENTIAL. AUTHORIZED USE ONLY. DO NOT REDISTRIBUTE.
- */
-
-// required by mapper-models/protobufjs
-type Long = number
