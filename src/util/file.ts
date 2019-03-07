a177e5f1318b21a3360aab0b28580ab320e22172
Clyde Brown <clyde@mapper.ai>
2019-03-07 12:47:17 -0800

    lint fixes

--- src/util/file.ts
+++ src/util/file.ts
@@ -4,7 +4,7 @@ export type Content = StrictUnion<ArrayBuffer | ArrayBufferView | Blob | string>
  * Opens a save dialog to save the given content of given mime type, with
  * default pre-filled filename.
  */
-export function saveFileWithDialog(content: Content, mime: string, filename: string) {
+export function saveFileWithDialog(content: Content, mime: string, filename: string): void {
   const file = new Blob([content], {type: mime})
   const url = URL.createObjectURL(file)
   const a = document.createElement('a')
